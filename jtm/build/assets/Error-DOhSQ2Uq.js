82df85c57f489288617e5dcbcab85643b9e9c93f
Onward <worukaonward@gmail.com>
2024-11-20 11:02:38 +0100

    commit

--- jtm/build/assets/Error-DOhSQ2Uq.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-upWJT3dq.js";function t(){return r.jsx("div",{children:r.jsx("h1",{children:"Not Found"})})}export{t as default};
--- jtm/src/App.jsx
+++ jtm/src/App.jsx
@@ -1,39 +1,34 @@
-import React, { Suspense, lazy } from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
 import ScrollToTop from "./components/ScroolToTop/ScrollToTop";
 import ScrollToTopButton from './components/ScroolToTop/ScrollToTopButton';
 
-
-const Home = lazy(() => import("./pages/Home/Home"));
-const AboutPage = lazy(() => import("./pages/AboutPage/AboutPage"));
-const Featured = lazy(() => import("./pages/Featured/Featured"));
-const Contact = lazy(() => import("./pages/Contact/Contact"));
-const Error = lazy(() => import("./components/Error/Error"));
-const Services = lazy(() => import("./pages/Services/Services"));
-const TheYellowBlogPage = lazy(() => import("./pages/TheYellowBlogPage/TheYellowBlogPage"))
-const FullBlogPost = lazy(() => import("./pages/FullBlogPost/FullBlogPost"))
-
-
+import Home from "./pages/Home/Home";
+import AboutPage from "./pages/AboutPage/AboutPage";
+import Featured from "./pages/Featured/Featured";
+import Contact from "./pages/Contact/Contact";
+import Error from "./components/Error/Error";
+import Services from "./pages/Services/Services";
+import TheYellowBlogPage from "./pages/TheYellowBlogPage/TheYellowBlogPage";
+import FullBlogPost from "./pages/FullBlogPost/FullBlogPost";
 
 function App() {
   return (
     <BrowserRouter>
-      <Suspense fallback={<div>Loading...</div>}>
-        <NavBar/>
-        <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/featured" element={<Featured />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/services" element={<Services/>} />
-          <Route path="/blogs/:slug" element={<FullBlogPost />} />
-          <Route path="/blog-posts" element={<TheYellowBlogPage />} />
-          <Route path="*" element={<Error />} />
-        </Routes>
-        <ScrollToTopButton/>
-      </Suspense>
+      <NavBar />
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/featured" element={<Featured />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/blogs/:slug" element={<FullBlogPost />} />
+        <Route path="/blog-posts" element={<TheYellowBlogPage />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <ScrollToTopButton />
     </BrowserRouter>
   );
 }
