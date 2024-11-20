32d2cf8bfd00895a63b12bead59ce201fbdd2790
Onward <worukaonward@gmail.com>
2024-11-20 11:08:43 +0100

    commit

--- jtm/src/App.jsx
+++ jtm/src/App.jsx
@@ -1,34 +1,39 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
 import ScrollToTop from "./components/ScroolToTop/ScrollToTop";
 import ScrollToTopButton from './components/ScroolToTop/ScrollToTopButton';
 
-import Home from "./pages/Home/Home";
-import AboutPage from "./pages/AboutPage/AboutPage";
-import Featured from "./pages/Featured/Featured";
-import Contact from "./pages/Contact/Contact";
-import Error from "./components/Error/Error";
-import Services from "./pages/Services/Services";
-import TheYellowBlogPage from "./pages/TheYellowBlogPage/TheYellowBlogPage";
-import FullBlogPost from "./pages/FullBlogPost/FullBlogPost";
+
+const Home = lazy(() => import("./pages/Home/Home"));
+const AboutPage = lazy(() => import("./pages/AboutPage/AboutPage"));
+const Featured = lazy(() => import("./pages/Featured/Featured"));
+const Contact = lazy(() => import("./pages/Contact/Contact"));
+const Error = lazy(() => import("./components/Error/Error"));
+const Services = lazy(() => import("./pages/Services/Services"));
+const TheYellowBlogPage = lazy(() => import("./pages/TheYellowBlogPage/TheYellowBlogPage"))
+const FullBlogPost = lazy(() => import("./pages/FullBlogPost/FullBlogPost"))
+
+
 
 function App() {
   return (
     <BrowserRouter>
-      <NavBar />
-      <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/featured" element={<Featured />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/blogs/:slug" element={<FullBlogPost />} />
-        <Route path="/blog-posts" element={<TheYellowBlogPage />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-      <ScrollToTopButton />
+      <Suspense fallback={<div>Loading...</div>}>
+        <NavBar/>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/featured" element={<Featured />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/services" element={<Services/>} />
+          <Route path="/blogs/:slug" element={<FullBlogPost />} />
+          <Route path="/blog-posts" element={<TheYellowBlogPage />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+        <ScrollToTopButton/>
+      </Suspense>
     </BrowserRouter>
   );
 }
